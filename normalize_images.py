bc3ce660ad945f21bf2de577a3d6425c455e993b
C-Radius <deus.deceit@gmail.com>
2022-11-04 01:01:06 +0200

    Finalizing command line arguments. Handling for single file added in premature state.

--- normalize_images.py
+++ normalize_images.py
@@ -105,17 +105,21 @@ def scale_to_fit(img, img_size, padding):
     paste_pos_x = math.ceil((width/2)-(new_size_x/2))
     paste_pos_y = math.ceil((height/2)-(new_size_y/2))
     result.paste(img, (paste_pos_x, paste_pos_y))
-    result.save(os.path.join("output", os.path.basename(f)), "JPEG")
+    if show_color_result:
+        result.show()
     return result
+
 
 def usage():
     pass
 
 
-show_grayscale_result = False
-save_extension = ".jpg"
 mark_collisions = False
-enable_logging = True
+enable_logging = False
+show_grayscale_result = False
+show_color_result = False
+force_replace = False
+save_extension = ".jpg"
 image_size = (0, 0)
 padding = 0
 threshold = 5
@@ -124,28 +128,34 @@ output_f = os.getcwd()
 directory = os.getcwd()
 
 if __name__ == "__main__":
+    print(sys.argv[1:])
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hsioetp:lgc", [
-                                   "help", "size=", "if=", "of=", "extension=", "threshold=", "padding="])
+        opts, args = getopt.getopt(sys.argv[1:], "hs:i:o:e:t:p:lgmcr", [
+                                   "help", "size=", "if=", "of=", "ext=", "threshold=", "padding="])
     except getopt.GetoptError as err:
         print(err)
         usage()
         sys.exit(2)
 
     for o, a in opts:
+        print(o, a)
         if o == "-l":
             enable_logging = True
         elif o == "-g":
             show_grayscale_result = True
-        elif o == "-c":
+        elif o == "-m":
             mark_collisions = True
+        elif o == "-c":
+            show_color_result = True
+        elif o == "-r":
+            force_replace = True
         elif o in ("-h", "--help"):
             usage()
             sys.exit(2)
-        elif o in ("-s", "--src"):
-            source_f = a
-        elif o in ("-d", "--dest"):
-            destination_f = a
+        elif o in ("-i", "--if"):
+            input_f = os.path.join(os.getcwd(), a)
+        elif o in ("-o", "--of"):
+            output_f = os.path.join(os.getcwd(), a)
         elif o in ("-e", "--ext"):
             save_extension = a
         elif o in ("-t", "--threshold"):
@@ -153,22 +163,30 @@ if __name__ == "__main__":
         elif o in ("-p", "--padding"):
             padding = int(a)
         elif o in ("-s", "--size"):
-            image_size = tuple(a)
+            image_size = tuple(int(x) for x in a.split(" "))
 
     logging.basicConfig(filename='journal.log',
                         encoding='utf-8', level=logging.INFO)
     logging.getLogger().addHandler(logging.StreamHandler())
 
-    with contextlib.suppress(Exception):
-        os.mkdir("output")
-
+    # Check to see if we're handling single file or folder
     if os.path.isfile(input_f):
-        pass
+        img = Image.open(input_f)
+        img = scale_to_fit(img, image_size, 60)
+        img.save(os.path.join(os.getcwd(), output_f))
+        img.close()
+
     elif os.path.isdir(input_f):
-        directory = os.path.join(os.getcwd(), input_f)
-        for index, filename in enumerate(os.listdir(directory)):
-            f = os.path.join(directory, filename)
-            if os.path.isfile(f) and f.endswith((save_extension)) and not os.path.exists(os.path.join(os.getcwd(), output_f, filename)):
+        # if it's not a file, then it has to be a folder so we try to create the output location
+        with contextlib.suppress(Exception):
+            os.mkdir(output_f)
+
+        for index, filename in enumerate(os.listdir(input_f)):
+            f = os.path.join(input_f, filename)
+            if os.path.isfile(f) and f.endswith((save_extension)):
+                if os.path.exists(os.path.join(output_f, filename)) and not force_replace:
+                    continue
+
                 if enable_logging:
                     print(index)
                 img = Image.open(f)

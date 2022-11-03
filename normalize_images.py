fe33e5f3a324f96414f01ab7d784b365f6a95a4b
C-Radius <deus.deceit@gmail.com>
2022-11-03 21:09:55 +0200

    Adding command line arguments. Plus support for single image or entire folder processing

--- normalize_images.py
+++ normalize_images.py
@@ -1,15 +1,11 @@
 import os
+import sys
 import PIL
 import math
 import logging
 import contextlib
+import getopt
 from PIL import Image, ImageOps
-
-SHOW_GRAYSCALE_RESULT = False
-MARK_COLLISIONS = False
-ENABLE_LOGGING = True
-
-directory = os.path.join(os.getcwd(), "source")
 
 
 def image_boundbox(img, bg_color=(255, 255, 255), tolerance=5):
@@ -24,7 +20,7 @@ def image_boundbox(img, bg_color=(255, 255, 255), tolerance=5):
     for y in range(height):
         for x in range(width):
             if img_grayscale.getpixel((x, y)) not in tolerance_range:
-                if MARK_COLLISIONS:
+                if mark_collisions:
                     img_grayscale.putpixel((x, y), (0))
                 if (x > left and left == 0) or (x < left and left != 0):
                     left = x
@@ -34,7 +30,7 @@ def image_boundbox(img, bg_color=(255, 255, 255), tolerance=5):
     for y in range(height):
         for x in reversed(range(width)):
             if img_grayscale.getpixel((x, y)) not in tolerance_range:
-                if MARK_COLLISIONS:
+                if mark_collisions:
                     img_grayscale.putpixel((x, y), (0))
                 if x < right and right == width or x > right and right != width:
                     right = x
@@ -44,7 +40,7 @@ def image_boundbox(img, bg_color=(255, 255, 255), tolerance=5):
     for x in range(width):
         for y in range(height):
             if img_grayscale.getpixel((x, y)) not in tolerance_range:
-                if MARK_COLLISIONS:
+                if mark_collisions:
                     img_grayscale.putpixel((x, y), (0))
                 if y > top and top == 0 or y < top and top != 0:
                     top = y
@@ -54,13 +50,13 @@ def image_boundbox(img, bg_color=(255, 255, 255), tolerance=5):
     for x in range(width):
         for y in reversed(range(height)):
             if img_grayscale.getpixel((x, y)) not in tolerance_range:
-                if MARK_COLLISIONS:
+                if mark_collisions:
                     img_grayscale.putpixel((x, y), (0))
                 if y < bottom and bottom == height or y > bottom and bottom != height:
                     bottom = y
                 break
 
-    if SHOW_GRAYSCALE_RESULT:
+    if show_grayscale_result:
         img_grayscale.show()
 
     img_grayscale.close()
@@ -68,8 +64,8 @@ def image_boundbox(img, bg_color=(255, 255, 255), tolerance=5):
     return (left, top, right, bottom)
 
 
-def scale_to_fit(filename, width, height, padding):
-    img = Image.open(filename)
+def scale_to_fit(img, img_size, padding):
+    width, height = img_size
     image_width, image_height = img.size
 
     if image_width > width or image_height > height:
@@ -91,7 +87,7 @@ def scale_to_fit(filename, width, height, padding):
         increment = new_size_y - actual_height
         new_size_x = actual_width + increment
 
-    if ENABLE_LOGGING:
+    if enable_logging:
         logging.info('Image: %s ------------------',
                      os.path.basename(filename))
         logging.info('actual_width: %d - actual_height: %d',
@@ -113,7 +109,52 @@ def scale_to_fit(filename, width, height, padding):
     result.close()
 
 
+def usage():
+    pass
+
+
+show_grayscale_result = False
+mark_collisions = False
+enable_logging = True
+image_size = (0, 0)
+padding = 0
+threshold = 5
+input_f = os.getcwd()
+output_f = os.getcwd()
+directory = os.getcwd()
+
 if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hsioetp:lgc", [
+                                   "help", "size=", "if=", "of=", "extension=", "threshold=", "padding="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o == "-l":
+            enable_logging = True
+        elif o == "-g":
+            show_grayscale_result = True
+        elif o == "-c":
+            mark_collisions = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(2)
+        elif o in ("-s", "--src"):
+            source_f = a
+        elif o in ("-d", "--dest"):
+            destination_f = a
+        elif o in ("-e", "--ext"):
+            save_extension = a
+        elif o in ("-t", "--threshold"):
+            threshold = int(a)
+        elif o in ("-p", "--padding"):
+            padding = int(a)
+        elif o in ("-s", "--size"):
+            image_size = tuple(a)
+
     logging.basicConfig(filename='journal.log',
                         encoding='utf-8', level=logging.INFO)
     logging.getLogger().addHandler(logging.StreamHandler())
@@ -121,11 +162,19 @@ if __name__ == "__main__":
     with contextlib.suppress(Exception):
         os.mkdir("output")
 
-    for index, filename in enumerate(os.listdir(directory)):
-        if ENABLE_LOGGING:
-            print(index)
-
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f) and f.endswith((".jpg")) and not os.path.exists(os.path.join(os.getcwd(), "output", filename)):
-            scale_to_fit(os.path.join(
-                os.getcwd(), "source", filename), 800, 800, 60)
+    if os.path.isfile(input_f):
+        pass
+    elif os.path.isdir(input_f):
+        directory = os.path.join(os.getcwd(), input_f)
+        for index, filename in enumerate(os.listdir(directory)):
+            if enable_logging:
+                print(index)
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f) and f.endswith((save_extension)) and not os.path.exists(os.path.join(os.getcwd(), output_f, filename)):
+                img = Image.open(f)
+                img = scale_to_fit(img, image_size, 60)
+                img.save(os.path.join(os.getcwd(), output_f, filename))
+                img.close()
+    else:
+        usage()
+        sys.exit(2)
